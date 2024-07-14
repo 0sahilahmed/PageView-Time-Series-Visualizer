6914694df40c08051e2b220f5bc843f4a92df178
0sahilahmed <57229974+0sahilahmed@users.noreply.github.com>
2024-07-14 17:24:12 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is the main script to test your functions
+
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+
+# Test the functions and save the plots
+draw_line_plot()
+draw_bar_plot()
+draw_box_plot()
